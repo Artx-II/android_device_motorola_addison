a814918d29996bad2977f81f29d535988a1e8466
Alberto97 <albertop2197@gmail.com>
2018-04-14 22:17:33 +0200

    Move properties to a dedicated makefile
    In AOSP there is not a way to append a text file to vendor/build.prop unlike the system counterpart.
    Move everything under PRODUCT_PROPERTY_OVERRIDES that will take care of adding the properties in the right partition.
    Also keep the makefile name generic since its content might go in different places.
    
    2019-02-10: Update to match system.prop to date

--- device.mk
+++ device.mk
@@ -317,5 +317,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
+include $(LOCAL_PATH)/device_prop.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
