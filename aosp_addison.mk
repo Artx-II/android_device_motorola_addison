0a55697a91be6f8481f644258a69335d24a0dd53
Jesus Acosta <jesusgabriel.91@gmail.com>
2019-05-21 23:19:11 -0400

    addison: add target screen height and width

--- aosp_addison.mk
+++ aosp_addison.mk
@@ -24,6 +24,10 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 $(call inherit-product, device/motorola/addison/device.mk)
 $(call inherit-product-if-exists, vendor/motorola/addison/addison-vendor.mk)
 
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 800
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := addison
 PRODUCT_NAME := aosp_addison
